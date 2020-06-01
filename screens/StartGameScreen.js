b3c81c2e533c952567b1e2c0bccdd7ae91aa9eec
Alex <alex1mosh@gmail.com>
2020-06-01 06:52:13 +0300

    WIP

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
 import Card from '../components/Card';
+import Color from '../constants/colors';
+import Input from '../components/Input';
 
 const StartGameScreen = props => {
     return (
@@ -8,10 +11,14 @@ const StartGameScreen = props => {
             <Text style={ styles.title }>Start a New Game!</Text>
             <Card style={ styles.inputContanier }>
                 <Text>Select a Number</Text>
-                <TextInput/>
+                <Input style={ styles.input }/>
                 <View style={ styles.buttonContanier }>
-                    <Button title="Reset" onPress={ ()=> {} } />
-                    <Button title="Confirm" onPress={ ()=> {} } />
+                    <View style={ styles.button }>
+                        <Button title="Reset" onPress={ ()=> {} } color={ Color.accent } />
+                    </View>
+                    <View style={ styles.button }>
+                        <Button title="Confirm" onPress={ ()=> {} } color={ Color.primary } />
+                    </View>
                 </View>
             </Card>
         </View>
@@ -38,6 +45,12 @@ const styles = StyleSheet.create({
         width: '100%',
         justifyContent: 'space-between',
         paddingHorizontal: 15,
+    },
+    button: {
+        width: 100,
+    },
+    input: {
+        width: 50,
     },
 });
 
